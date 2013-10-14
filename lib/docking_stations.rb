6b3c6919466af54dc3ebf038a4e688b6550a8756
Simon Whittaker <whittakersc@gmail.com>
2013-10-14 22:26:49 +0100

    docking station progress

---
+++ lib/docking_stations.rb
@@ -0,0 +1,18 @@
+class DockingStation
+
+  def initialize
+  	@bikes = []
+  end
+
+  def bike_count
+      @bikes.count
+  end
+
+  def dock(bike)
+  	@bikes << bike
+  end
+
+  def release(bike)
+  	@bikes.delete(bike)
+  end
+end
